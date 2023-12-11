849e3139850b5462ff557599d33f55c69d562ed1
Bharath <f20180724@pilani.bits-pilani.ac.in>
2023-12-11 16:35:31 +0530

    added login/register routes and jwt tokenization

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt')
+const jwt = require ('jsonwebtoken')
 const dotenv = require("dotenv");
 dotenv.config();
 
-const User= require('./models/userModel')
+const User= require('./models/userModel');
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 const app = express();
 
@@ -17,19 +19,29 @@ app.get("/health", (req, res) => {
   res.json({
     status: "success",
     message: "server is up and running",
+    time: new Date()
   });
 });
 
-app.post('/signup',async(req,res) => {
+app.post('/register',async(req,res) => {
     try {
         const {name,email,phone,password}=req.body;
-        const encryptedPassword= await bcrypt.hash(password,10)
-        console.log(encryptedPassword)
-        await User.create({name,email,phone,password:encryptedPassword})  
-        res.json({
+        //check user with same email already exists
+        const user= await User.findOne({email})        
+        if(!(user)){
+            const encryptedPassword= await bcrypt.hash(password,10)
+            await User.create({name,email,phone,password:encryptedPassword})  
+            res.json({
             status:'successful',
             message:'user created successfully'
            }) 
+        }
+        else{
+            res.json({
+                status : 'unsuccessful',
+                message: 'email already registered'
+            })
+        }
     } 
     catch (error) {
         console.log(error)
@@ -37,6 +49,43 @@ app.post('/signup',async(req,res) => {
         status:'unsuccessful',
         message:'user not created successfully'
        }) 
+    }
+})
+
+app.post('/login',async (req,res)=>{
+    try {
+        const {email,password}=req.body;
+        let isValidUser = await User.findOne({email});
+        if(isValidUser){
+            let isPasswordCorrect = await bcrypt.compare(password,isValidUser.password)
+            if(isPasswordCorrect){
+                const jwtoken = jwt.sign(isValidUser.toJSON(),process.env.jwtSecretKey,{expiresIn:60*60*24})
+                res.json({
+                    status:'successful',
+                    message:'logged in successfully',
+                    jwtoken
+                })
+            }
+            else{
+                res.json({
+                    status:'error',
+                    message: 'incorrect password'
+                })
+            }
+        }
+        else{
+            res.json({
+                status:'error',
+                message:'email doesnot exist'
+            })
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.json({
+            status:'error',
+            message:'login failed'
+        })
     }
 })
 
